3956bd5b317fad9b001cdeeca4c55acc3cc5d314
Ian McBurnie <ianmcburnie@hotmail.com>
2015-07-22 11:20:41 -0700

    Added options.cursorDirection

--- jquery.rovingtabindex.js
+++ jquery.rovingtabindex.js
@@ -1,56 +1,85 @@
 /**
-* rovingtabindex.js
-* @author Ian McBurnie
-*
-* Implements a roving tabindex on a collection of elements.
-* http://www.w3.org/TR/wai-aria-practices/#kbd_general_within
-*
-* Triggers 'rovingtabindex' event when tabindex changes
-*
-* @dependencies jquery.keyhandlers.js
+* @function jquery.rovingtabindex.js
+* @author Ian McBurnie <ianmcburnie@hotmail.com>
+* @description Implements a roving tabindex on a collection of elements.
+* @summary http://www.w3.org/TR/wai-aria-practices/#kbd_general_within
+* @requires jquery.keyhandlers.js
+* @param {string} id
+* @param {Object} options
+* @param {string} options.cursorDirection
+* @fires rovingtabindex
 */
-
-(function ( $ ) {
+(function ($, window, document, undefined) {
 
     $.fn.rovingtabindex = function rovingTabIndex(id, options) {
 
-        var _idx = 0;
+        var _idx = 0,
+            options = options || {},
+            wrap = options.wrap,
+            cursorDirection = options.cursorDirection || 'both';
 
-        options = options || {};
-
-        return this.each(function onEach() {
+        return this.each(function onEach(i) {
 
             var $this = $(this);
 
             // store collection index pos in element dataset
             $this.attr('data-{0}'.replace('{0}', id), JSON.stringify({'rovingtabindex': _idx++}));
 
-            $this.onLeftOrUpArrowKey(function onLeftOrUpArrowKey() {
+            if (cursorDirection === 'horizontal') {
+                $this.on('leftkeydown', function onLeftArrowKey() {
+                    $this.trigger('prev');
+                });
+
+                $this.on('rightkeydown', function onRightArrowKey() {
+                    $this.trigger('next');
+                });
+            }
+            else if (cursorDirection === 'vertical') {
+                $this.on('downkeydown', function onDownArrowKey() {
+                    $this.trigger('next');
+                });
+
+                $this.on('upkeydown', function onUpArrowKey() {
+                    $this.trigger('prev');
+                });
+            }
+            else {
+                $this.on('leftkeydown upkeydown', function onLeftOrUpArrowKey() {
+                    $this.trigger('prev');
+                });
+
+                $this.on('rightkeydown downkeydown', function onRightOrDownArrowKey() {
+                    $this.trigger('next');
+                });
+            }
+
+            $this.on('prev', function onPrev(e) {
                 var itemIdx = $this.data(id).rovingtabindex,
                     $siblings = $('[data-{0}]'.replace('{0}', id)),
                     $prevEl = $siblings.eq(itemIdx - 1),
                     hasPrevEl = $prevEl.length === 1,
                     $lastEl = $siblings.eq($siblings.length-1),
-                    $roveToEl = (hasPrevEl) ? $prevEl : $lastEl;
+                    $roveToEl = (hasPrevEl && itemIdx !== 0) ? $prevEl : (options.wrap !== false) ? $lastEl : $this;
 
                 $this.attr('tabindex', '-1');
                 $roveToEl.attr('tabindex', '0');
                 $this.trigger('rovingtabindex', $roveToEl);
             });
 
-            $this.onRightOrDownArrowKey(function onRightOrDownArrowKey() {
+            $this.on('next', function onNext(e) {
                 var itemIdx = $this.data(id).rovingtabindex,
                     $siblings = $('[data-{0}]'.replace('{0}', id)),
                     $nextEl = $siblings.eq(itemIdx + 1),
                     hasNextEl = $nextEl.length === 1,
                     $firstEl = $siblings.eq(0),
-                    $roveToEl = (hasNextEl) ? $nextEl : $firstEl;
+                    $roveToEl = (hasNextEl) ? $nextEl : (options.wrap !== false) ? $firstEl: $this;
 
                 $this.attr('tabindex', '-1');
                 $roveToEl.attr('tabindex', '0');
                 $this.trigger('rovingtabindex', $roveToEl);
             });
+
         });
     };
 
-}( jQuery ));
+}(jQuery, window, document));
